4e922995f5fcd58b917fdf454bb4dc7df851286f
Victor <vicsamnav2@alu.edu.gva.es>
2022-11-28 21:29:59 +0100

    ejercicios7-8

---
+++ Matrices/Ejercicio 7.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+int[,] aforoSalas = new int[3, 3]
+{
+    {200, 200, 200},
+    {150, 150, 150},
+    {125, 125, 125}
+};
+int entradasVendidas;
+string salaElegida;
+int numSesion;
+int eleccion;
+do
+{
+
+    Console.WriteLine("1-Venta de entradas");
+    Console.WriteLine("2-Estadistica de aforo");
+    eleccion = int.Parse(Console.ReadLine());
+    switch (eleccion)
+    {
+        case 1:
+            Console.WriteLine("¿Cuantas entradas quieres?");
+            entradasVendidas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Elige una sala: SalaA, SalaB, SalaC");
+            salaElegida = Console.ReadLine().ToUpper();
+            int numSala = salaElegida switch
+            {
+                "SALAA" => 0,
+                "SALAB" => 1,
+                "SALAC" => 2,
+
+            };
+            Console.WriteLine("Elige la sesion: 1,2,3");
+            numSesion = int.Parse(Console.ReadLine()) - 1;
+
+            if (aforoSalas[numSala, numSesion] <= entradasVendidas)
+            {
+                Console.WriteLine("No quedan entradas para esa sesion en esa sala");
+            }
+            else
+            {
+                aforoSalas[numSala, numSesion] -= entradasVendidas;
+            }
+            break;
+        case 2:
+            for (int i = 0; i < aforoSalas.GetLength(0); i++)
+            {
+                for (int j = 0; j < aforoSalas.GetLength(1); j++)
+                {
+                    Console.Write(aforoSalas[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+            break;
+    }
+Console.WriteLine("Presiona ESC si quieres salir");
+} while (Console.ReadKey().Key!= ConsoleKey.Escape);
+
+
---
+++ Matrices/Ejercicio8.cs
@@ -0,0 +1,44 @@
+﻿
+Random generador = new Random();
+int nNumeros = 0;
+int lineas = 0;
+int[][][] m = new int[2][][];
+
+for(int i=0;i<m.Length;i++)
+{
+    Console.WriteLine("Dime el numero de lineas");
+    lineas = int.Parse(Console.ReadLine());
+    Array.Resize(ref m[i],lineas);
+
+    for(int j=0;j<m[i].Length;j++)
+    {
+            Array.Resize(ref m[i][j],nNumeros);
+            Console.WriteLine("Dime el numero de numeros");
+            nNumeros = int.Parse(Console.ReadLine());
+        for(int x=0;x<m[i][j].Length;x++)
+        {
+            m[i][j][x] = generador.Next(0,10);
+            
+        }
+        
+    }
+   
+}
+
+for(int i=0;i<m.Length;i++)
+{
+
+    for(int j=0;j<m[i].Length;j++)
+    {
+    
+        for(int x=0;x<m[i][j].Length;x++)
+        {
+            Console.Write(m[i][j][x]+" ");
+            
+        }
+        Console.WriteLine("");
+    }
+    Console.WriteLine("");
+}
+
+
